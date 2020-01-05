ac6507e44ba8b1537d969a6eaad5636baa6c5336
saddalmatian <saddalmatian@gmail.com>
2020-01-06 00:35:55 +0700

    commit

--- Test1.cpp
+++ Test1.cpp
@@ -16,7 +16,7 @@ int main(){
         main_table[i][j]='-';
         sub_table[i][j]='-';
     }
-    int change[size*size];
+    char change[size*size];
     //Chuyển sang 1 chiều
     int k=0;
     for(int i=0;i<size;i++){
@@ -25,5 +25,41 @@ int main(){
             k++;
         }
     }
-    
+ 
+    srand(time(NULL));
+    for(int d=0;d<mine;d++){
+int x = rand() % (size*size)-1 + 1;
+    change[x]='m';
+    }
+       int dem=0;    
+   while(dem!=mine){
+       
+       for(int d=0;d<size*size;d++){
+           if(change[d]=='m')
+           dem++;
+       }
+       if(dem!=mine){
+        int x = rand() % (size*size)-1 + 1;
+        change[x]='m';
+           dem=0;
+       }
+   }
+//chuyen sang 2 chieu
+k=0;
+    for(int i=0;i<size;i++){
+        for(int j=0;j<size;j++){
+            sub_table[i][j]=change[k];
+        k++;
+        }
+    }
+
+
+
+for(int i=0;i<size;i++){
+    for(int j=0;j<size;j++){
+        cout<<sub_table[i][j]<<" ";
+    }
+    cout<<"\n";
 }
+
+}//end of int main()

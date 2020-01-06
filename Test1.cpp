8881b3f00a453ce0a0a18873869573702fc072f8
saddalmatian <saddalmatian@gmail.com>
2020-01-07 00:30:03 +0700

    commit

--- Test1.cpp
+++ Test1.cpp
@@ -43,7 +43,7 @@ int x = rand() % (size*size)-1 + 1;
            dem=0;
        }
    }
-//chuyen sang 2 chieu
+    //chuyển sang 2 chiều
 k=0;
     for(int i=0;i<size;i++){
         for(int j=0;j<size;j++){
@@ -51,15 +51,7 @@ k=0;
         k++;
         }
     }
-//in truoc khi set so
-for(int i=0;i<size;i++){
-    for(int j=0;j<size;j++){
-        cout<<sub_table[i][j]<<" ";
-        
-    }
-    cout<<"\n";
-}
-//set so cho sub_table
+//set số cho sub_table
 for(int i=0;i<size;i++)
      for(int j=0;j<size;j++){
             int t1,t2;
@@ -93,7 +85,7 @@ for(int i=0;i<size;i++)
                     for(t2=(j-1);t2<(j+2);t2++)
                         if(sub_table[t1][t2]=='m')dem++;
             }
-             if(j==0){
+            else if(j==0){
                 for(t1=(i-1);t1<(i+2);t1++)
                     for(t2=j;t2<j+2;t2++)
                         if(sub_table[t1][t2]=='m')dem++;
@@ -140,15 +132,76 @@ for(int i=0;i<size;i++)
                  
                  }     
     
-
-//khó vl
-//in sau khi set so
+//in thử mảng phụ
 for(int i=0;i<size;i++){
     for(int j=0;j<size;j++){
         cout<<sub_table[i][j]<<" ";
         
     }
-    cout<<"\n";
-}
+    cout<<"\n";}
 
+// //chọn tọa độ x,y
+bool alive=true;
+while(alive){
+    int x,y;
+    //in mảng chính
+    for(int i=0;i<size;i++){
+        for(int j=0;j<size;j++){
+            cout<<main_table[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
+    cout<<"X(min=0,max="<<size-1<<"): ";cin>>x;
+    cout<<"Y(min=0,max="<<size-1<<"): ";cin>>y;
+    if(main_table[x][y]=='-'){
+        if(sub_table[x][y]=='m')alive=false;
+        else if(sub_table[x][y]!='0')
+            main_table[x][y]=sub_table[x][y];
+        else{
+            if(x==0 && y==0){
+                for(t1=x;t1<x+2;t1++)
+                    for(t2=y;t2<y+2;t2++)
+                        if(sub_table[t1][t2]=='0')main_table[t1][t2]=sub_table[t1][t2];
+            }
+                
+            else if(x==(size-1) && y==(size-1)){
+                for(t1=(x-1);t1<x+1;t1++)
+                    for(t2=(y-1);t2<y+1;t2++)
+                        if(sub_table[t1][t2]=='0')main_table[t1][t2]=sub_table[t1][t2];
+            }                      
+                
+            else if(x==(size-1) && y==0){
+                for(t1=(x-1);t1<x+1;t1++)
+                    for(t2=y;t2<y+2;t2++)
+                        if(sub_table[t1][t2]=='0')main_table[t1][t2]=sub_table[t1][t2];
+            }
+
+            else if(x==0 && y==(size-1)){
+                for(t1=x;t1<x+2;t1++)
+                    for(t2=(y-1);t2<y+1;t2++)
+                        if(sub_table[t1][t2]=='0')main_table[t1][t2]=sub_table[t1][t2];
+            }
+            else if(x==0){
+                for(t1=x;t1<x+2;t1++)
+                    for(t2=(y-1);t2<(y+2);t2++)
+                        if(sub_table[t1][t2]=='0')main_table[t1][t2]=sub_table[t1][t2];
+            }
+            else if(y==0){
+                for(t1=(x-1);t1<(x+2);t1++)
+                    for(t2=y;t2<y+2;t2++)
+                        if(sub_table[t1][t2]=='0')main_table[t1][t2]=sub_table[t1][t2];
+            }
+            else if(x!=0 && x!=(size-1) && y!=0 && y!=(size-1)){
+                for(t1=(x-1);t1<(x+2);t1++)
+                    for(t2=(y-1);t2<(y+2);t2++)
+                        if(sub_table[t1][t2]=='0')main_table[t1][t2]=sub_table[t1][t2];
+            }
+
+        }
+   
+    }//kết thúc điểu kiện main_table[x][y]='-'
+
+    else cout<<"Enter again! you chose this position before !";
+}
+    if(alive==false)cout<<"YOU LOSE!";
 }//end of int main()
